d4e7448e0af69e972644f2e379100d40d3b84bd7
Frost7994 <tommybyrne9@gmail.com>
2024-09-25 14:13:50 +0100

    lib(shadcn): added the base setup for shadcn

---
+++ src/components/contactButton.js
@@ -0,0 +1,8 @@
+// components
+import { Button } from "./ui/button";
+
+const ContactButton = () => {
+  return <Button className="bg-red-500 hover:bg-blue-400">Contact Me</Button>;
+};
+
+export default ContactButton;
--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -1,12 +1,13 @@
-// utils
-import cn from "@/lib/utils/cn";
+// components
+import { Button } from "@/components/ui/button";
+import ContactButton from "../contactButton";
 
 const Navbar = () => {
   return (
-    <nav className="fixed inset-x-0 top-0 w-full z-10 h-16 bg-red-500 text-white">
+    <nav className="fixed inset-x-0 top-0 w-full z-10 h-16 bg-background">
       <div className="container flex items-center justify-between w-full h-full mx-auto px-4">
         <p>Logo</p>
-        <button>click me</button>
+        <Button>click me</Button>
       </div>
     </nav>
   );
