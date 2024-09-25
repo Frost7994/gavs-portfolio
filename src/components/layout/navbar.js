7d76797a785718d3c42b3a04f21b816f12a2fd64
Frost7994 <tommybyrne9@gmail.com>
2024-09-25 14:35:10 +0100

    revert(themeButton): removed example theme button from project

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -1,14 +1,12 @@
 // components
 import { Button } from "@/components/ui/button";
-import ThemeButton from "../themeButton";
 
 const Navbar = () => {
   return (
     <nav className="fixed border-b inset-x-0 top-0 w-full z-10 h-16 bg-background">
       <div className="container flex items-center justify-between w-full h-full mx-auto px-4">
         <p>Logo</p>
-        {/* <Button>Click Me</Button> */}
-        <ThemeButton />
+        <Button>Click Me</Button>
       </div>
     </nav>
   );
